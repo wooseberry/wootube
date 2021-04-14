44ef5100176be24824f35d616ac9a42987c32038
wooseberry <wooseberry@gmail.com>
2021-04-15 08:29:38 +0900

    passport2

--- passport.js
+++ passport.js
@@ -11,8 +11,9 @@ passport.use(
         {
             clientID: process.env.GH_ID,
             clientSecret: process.env.GH_SECRET,
-            callbackURL: `http://localhost:4000${routes.githubCallback}`
+            redirect_uri: `http://localhost:4000${routes.githubCallback}`
         },
+
         githubLoginCallback
     )
 );
